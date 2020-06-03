ed71c04955c858f055b55552221e2affb0d32201
sripadas-cloud <sripadas@outlook.com>
2020-06-04 00:16:02 +0530

    updated for jenkins pl

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
 version '0.2.0'
-chef_version '>= 13.0
+chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when

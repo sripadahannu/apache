5539ef4ab123823e93afbfaee3b0f1750d44b31d
sripadas-cloud <sripadas@outlook.com>
2020-06-04 00:17:46 +0530

    updated for jenkins pl

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -20,11 +20,11 @@ describe 'apache::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
+  context 'When all attributes are default, on CentOS 7.6' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.6')
       runner.converge(described_recipe)
     end
 
